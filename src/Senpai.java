f79b66fd1f916e4472562007e85e85da97491f2b
adriendarki <adriendarki@outlook.fr>
2020-05-11 11:19:58 +0200

    push

---
+++ src/Senpai.java
@@ -0,0 +1,9 @@
+public class Senpai {
+
+        public static void main(String[] args) {
+            // afficher Hello World! dans le terminal
+            System.out.println("Notice me Senpai");
+        }
+    }
+
+
